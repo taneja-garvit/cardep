35d94d7a5e6604141fbd188bdad79e48014798be
taneja-garvit <145535000+taneja-garvit@users.noreply.github.com>
2025-02-18 01:12:47 +0530

    Update

--- utils/index.ts
+++ utils/index.ts
@@ -46,7 +46,7 @@ export async function fetchCars(filters: FilterProps) {
 
   // Set the required headers for the API request
   const headers: HeadersInit = {
-    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "Ir/v3ZswrejCZvYcAHWdPw==icNzN0wYaVrp71ph",
+    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
